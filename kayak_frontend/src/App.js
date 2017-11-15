2212c46a10118f6c995b64806d19a45c9df01bc3
Aviral Kumar <aviralkum@gmail.com>
2017-11-15 10:42:22 +0530

    kayak header

--- kayak_frontend/src/App.js
+++ kayak_frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import ShowCars from './components/show_cars';
 import ShowFlights from './components/show_flights';
 import ShowHotels from './components/show_hotels';
+import Headers from './components/header';
 
 class App extends Component {
   render() {
@@ -12,7 +13,8 @@ class App extends Component {
       <BrowserRouter>
 
         <Switch>
-            <Route  exact path="/cardetails" render ={() => (<ShowCars/>)}/>
+            <Route  exact path="/cardetails" render ={() => (<Headers/>)}/>
+            
             <Route  exact path="/flightdetails" render ={() => (<ShowFlights/>)}/>
             <Route  exact path="/hoteldetails" render ={() => (<ShowHotels/>)}/>
 
