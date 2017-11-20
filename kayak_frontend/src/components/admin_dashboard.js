71756478287a10c19f8359d23d85fe2df504f117
akashgupta2903 <akash.gupta@gmail.com>
2017-11-19 17:46:19 -0800

    admin dasboard

---
+++ kayak_frontend/src/components/admin_dashboard.js
@@ -0,0 +1,63 @@
+import React,{ Component } from 'react';
+import AdminDashboardHeader from './headers/admin_dashboard_header';
+
+import './../images/home.css';
+
+class AdminDashboard extends Component {
+
+
+  render() {
+    console.log("It will render admin dash board page:");
+    return (
+            <div>
+                    <div className="admin-dashboard-header">
+                        <AdminDashboardHeader/>
+                    </div>
+
+                    <div className="admin-dashboard-welcome">
+                      <h2>Welcome to Dashboard</h2>
+                      <hr/>
+                    </div>
+
+                    <div className="admin-dashboard-tiles-div">
+                        <div className="admin-dashboard-tile-car">
+                            <h3>Total cars rented: </h3>
+                            <br></br>
+                            <br></br>
+                            <h1>1000</h1>
+                        </div>
+
+                        <div className="admin-dashboard-tile-flight">
+                            <h3>Total flights booked: </h3>
+                            <br></br>
+                            <br></br>
+                            <h1>1000</h1>
+                        </div>
+
+                        <div className="admin-dashboard-tile-hotel">
+                            <h3>Total hotels booked: </h3>
+                            <br></br>
+                            <br></br>
+                            <h1>1000</h1>
+                        </div>
+
+                        <div className="admin-dashboard-tile-users">
+                            <h3>Total users active: </h3>
+                            <br></br>
+                            <br></br>
+                            <h1>1000</h1>
+                        </div>
+
+                    </div>
+
+                    <div className="admin-dashboard-graph">
+                    </div>
+
+            </div>
+           );
+  }
+}
+
+
+
+export default AdminDashboard;
