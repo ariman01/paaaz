4b636909fb028d16bebb411a7e792498494096c1
akashgupta2903 <akash.gupta@gmail.com>
2017-11-20 01:27:31 -0800

    adding admin_dashboard_header

--- kayak_frontend/src/App.js
+++ kayak_frontend/src/App.js
@@ -14,7 +14,7 @@ import CarSearchLeftNav from './components/searchbars/car_search_leftnav';
 import CarTile from './components/searchbars/car_tiles';
 import FlightTile from './components/searchbars/flight_tiles';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-<<<<<<< HEAD
+
 import ViewCar from './components/subcomponents/view_car';
 import BookingDetails from './components/subcomponents/booking_details';
 import RentalCarPrice from './components/subcomponents/rental_car_price';
@@ -22,11 +22,13 @@ import CarBillingSummary from './components/subcomponents/car_billing_summary';
 import CarBillingPage from './components/car_billing_page';
 import FlightBillingPage from './components/flight_billing_page';
 import AdminDashboard from './components/admin_dashboard';
-//import Headers from './components/header';
+import AddCarAdmin from './components/subcomponents/add_car_admin';
+import AddHotelAdmin from './components/subcomponents/add_hotel_admin';
+import AddFlightAdmin from './components/subcomponents/add_flight_admin';
 
-import CarBillingSummary from './components/subcomponents/car_billing_summary';
-import CarBillingPage from './components/car_billing_page';
-import HotelTile from './components/searchbars/hotel_tiles';
+import CarGraphs from './components/subcomponents/car_graphs';
+
+
 
 class App extends Component {
   render() {
@@ -45,6 +47,10 @@ class App extends Component {
                     <Route  exact path="/" render ={() => (<CarBillingPage/>)}/>
                     <Route  exact path="/flightbillingpage" render ={() => (<FlightBillingPage/>)}/>
                     <Route  exact path="/admindashboard" render ={() => (<AdminDashboard/>)}/>
+                    <Route  exact path="/cargraphs" render ={() => (<CarGraphs/>)}/>
+                    <Route  exact path="/addcaradmin" render ={() => (<AddCarAdmin/>)}/>
+                    <Route  exact path="/addhoteladmin" render ={() => (<AddHotelAdmin/>)}/>
+                    <Route  exact path="/addflightadmin" render ={() => (<AddFlightAdmin/>)}/>
 
 
               </Switch>
