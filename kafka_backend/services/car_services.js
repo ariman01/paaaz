a25ba67ffe4411497786f9c674e69e7b484099a6
Pooja539 <32176046+Pooja539@users.noreply.github.com>
2017-11-21 00:33:41 -0800

    New registration code updated in kafkabackend

--- kafka_backend/services/car_services.js
+++ kafka_backend/services/car_services.js
@@ -1,77 +1,77 @@
 var Cars = require('./../models/cars')
 
 exports.searchCars = function(data, callback){
-  Cars.searchCars( data.src_city, data.destination_city , function(err , results){
-    if(err){
-      callback(null,err);
-    }
-    else{
-      callback(null,results);
-    }
-  });
+    Cars.searchCars( data.src_city, data.destination_city , function(err , results){
+        if(err){
+            callback(null,err);
+        }
+        else{
+            callback(null,results);
+        }
+    });
 }
 
 exports.addCar = function(data, callback){
-  var carDetail = new Cars.Cars({
-    model_no:data.model_no,
-    name:data.name,
-    capacity:data.capacity,
-    no_of_bags:data.no_of_bags,
-    no_of_doors:data.no_of_doors,
-    price:data.price,
-    src_city:data.src_city,
-    destination_city:data.destination_city,
-    rental_agency:data.rental_agency,
-    car_type:data.car_type}
-  );
-  Cars.addNewCar( carDetail , function(err , results){
-    if(err){
-      console.log("[Kafka] Error adding new car")
-    }
-    callback(err,results);
-  });
+    var carDetail = new Cars.Cars({
+        model_no:data.model_no,
+        name:data.name,
+        capacity:data.capacity,
+        no_of_bags:data.no_of_bags,
+        no_of_doors:data.no_of_doors,
+        price:data.price,
+        src_city:data.src_city,
+        destination_city:data.destination_city,
+        rental_agency:data.rental_agency,
+        car_type:data.car_type}
+    );
+    Cars.addNewCar( carDetail , function(err , results){
+        if(err){
+            console.log("[Kafka] Error adding new car")
+        }
+        callback(err,results);
+    });
 }
 
 exports.deleteCar = function(data, callback){
-  console.log("delete car data",data);
-  Cars.deleteCar( data.model_no , function(err , results){
-    if(err){
-      console.log("[Kafka] Error deleting car ")
-    }
-    callback(err,results);
-  });
+    console.log("delete car data",data);
+    Cars.deleteCar( data.model_no , function(err , results){
+        if(err){
+            console.log("[Kafka] Error deleting car ")
+        }
+        callback(err,results);
+    });
 }
 
 exports.editCar = function(data, callback){
-  console.log("edit car data",data);
-  if(!data.model_no){
-    callback("Model No missing, failed to edit without model_no",null);
-  }else{
-    var carEditData = {};
-    if(data.name)
-      carEditData.name = data.name;
-    if(data.capacity)
-        carEditData.capacity = data.capacity;
-    if(data.no_of_bags)
-        carEditData.no_of_bags = data.no_of_bags;
-    if(data.no_of_doors)
-        carEditData.no_of_doors = data.no_of_doors;
-    if(data.price)
-          carEditData.price = data.price;
-    if(data.src_city)
-        carEditData.src_city = data.src_city;
-    if(data.destination_city)
-        carEditData.destination_city = data.destination_city;
-    if(data.rental_agency)
-        carEditData.rental_agency = data.rental_agency;
-    if(data.car_type)
-        carEditData.car_type = data.car_type;
-    Cars.updateCar(data.model_no, carEditData, function(err,result){
-      if(err){
-        console.log("[Kafka] Error while editing car detail");
-      }
-      callback(err, result);
-    });
-  }
+    console.log("edit car data",data);
+    if(!data.model_no){
+        callback("Model No missing, failed to edit without model_no",null);
+    }else{
+        var carEditData = {};
+        if(data.name)
+            carEditData.name = data.name;
+        if(data.capacity)
+            carEditData.capacity = data.capacity;
+        if(data.no_of_bags)
+            carEditData.no_of_bags = data.no_of_bags;
+        if(data.no_of_doors)
+            carEditData.no_of_doors = data.no_of_doors;
+        if(data.price)
+            carEditData.price = data.price;
+        if(data.src_city)
+            carEditData.src_city = data.src_city;
+        if(data.destination_city)
+            carEditData.destination_city = data.destination_city;
+        if(data.rental_agency)
+            carEditData.rental_agency = data.rental_agency;
+        if(data.car_type)
+            carEditData.car_type = data.car_type;
+        Cars.updateCar(data.model_no, carEditData, function(err,result){
+            if(err){
+                console.log("[Kafka] Error while editing car detail");
+            }
+            callback(err, result);
+        });
+    }
 
 }
--- kafka_backend/services/flight_services.js
+++ kafka_backend/services/flight_services.js
@@ -2,76 +2,76 @@ var  Flights = require('./../models/flights');
 
 
 exports.searchFlights = function(data, callback){
-  Flights.searchFlights(data, function(err, result){
-    if(err){
-      console.log("[Kafka] Error while searching for ")
-    }
-    callback(err, result);
+    Flights.searchFlights(data, function(err, result){
+        if(err){
+            console.log("[Kafka] Error while searching for ")
+        }
+        callback(err, result);
 
-  });
+    });
 }
 
 exports.addFlight = function(data, callback){
-  var flight = new Flights.Flights({
-		flight_id:data.flight_id,
-		carrier_name:data.carrier_name,
-		src_city:data.src_city,
-		destination_city:data.destination_city,
-		flight_duration:data.flight_duration,
-		operational_day:data.operational_day,
-		departure_time:data.departure_time,
-		price:data.price
-  });
-  Flights.addNewFlight(flight, function(err, result){
-    if(err){
-      console.log("[Kafka] Error while adding new flight:",flight.carrier_name);
-    }
-    callback(err,result);
-  });
+    var flight = new Flights.Flights({
+        flight_id:data.flight_id,
+        carrier_name:data.carrier_name,
+        src_city:data.src_city,
+        destination_city:data.destination_city,
+        flight_duration:data.flight_duration,
+        operational_day:data.operational_day,
+        departure_time:data.departure_time,
+        price:data.price
+    });
+    Flights.addNewFlight(flight, function(err, result){
+        if(err){
+            console.log("[Kafka] Error while adding new flight:",flight.carrier_name);
+        }
+        callback(err,result);
+    });
 }
 
 exports.deleteFlight = function(data, callback){
-  console.log("delete flight data",data);
-  Flights.deleteFlight( data.flight_id , function(err , results){
-    if(err){
-      console.log("[Kafka] Error deleting car ")
-    }
-    callback(err,results);
-  });
+    console.log("delete flight data",data);
+    Flights.deleteFlight( data.flight_id , function(err , results){
+        if(err){
+            console.log("[Kafka] Error deleting car ")
+        }
+        callback(err,results);
+    });
 }
 
 exports.editFlight = function(data, callback){
-  console.log("edit car data",data);
-  if(!data.flight_id){
-    callback("Model No missing, failed to edit without model_no",null);
-  }else{
-    var flightData = {};
-    if(data.flight_id)
-      flightData.flight_id = data.flight_id;
-    if(data.carrier_name)
-        flightData.carrier_name = data.carrier_name;
-    if(data.src_city)
-        flightData.src_city = data.src_city;
-    if(data.destination_city)
-        flightData.destination_city = data.destination_city;
-    if(data.flight_duration)
-          flightData.flight_duration = data.flight_duration;
-    if(data.src_city)
-        flightData.src_city = data.src_city;
-    if(data.destination_city)
-        flightData.destination_city = data.destination_city;
-    if(data.operational_day)
-        flightData.operational_day = data.operational_day;
-    if(data.departure_time)
-        flightData.departure_time = data.departure_time;
-    if(data.price)
-        flightData.price = data.price;
-    Flights.editFlight(data.flight_id, flightData, function(err,result){
-      if(err){
-        console.log("[Kafka] Error while editing flight detail");
-      }
-      callback(err, result);
-    });
-  }
+    console.log("edit car data",data);
+    if(!data.flight_id){
+        callback("Model No missing, failed to edit without model_no",null);
+    }else{
+        var flightData = {};
+        if(data.flight_id)
+            flightData.flight_id = data.flight_id;
+        if(data.carrier_name)
+            flightData.carrier_name = data.carrier_name;
+        if(data.src_city)
+            flightData.src_city = data.src_city;
+        if(data.destination_city)
+            flightData.destination_city = data.destination_city;
+        if(data.flight_duration)
+            flightData.flight_duration = data.flight_duration;
+        if(data.src_city)
+            flightData.src_city = data.src_city;
+        if(data.destination_city)
+            flightData.destination_city = data.destination_city;
+        if(data.operational_day)
+            flightData.operational_day = data.operational_day;
+        if(data.departure_time)
+            flightData.departure_time = data.departure_time;
+        if(data.price)
+            flightData.price = data.price;
+        Flights.editFlight(data.flight_id, flightData, function(err,result){
+            if(err){
+                console.log("[Kafka] Error while editing flight detail");
+            }
+            callback(err, result);
+        });
+    }
 
 }
