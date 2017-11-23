accda4d2b9978624aeb0e718e59663ab249eae96
ARIJIT MANDAL <oori@ARIJITs-MacBook-Pro.local>
2017-11-22 16:20:42 -0800

    admin analysis report for hotel

--- kayak_frontend/src/actions/admin_action.js
+++ kayak_frontend/src/actions/admin_action.js
@@ -3,3 +3,11 @@ export const updateTotalSales = (total_Sales) =>{
   console.log("Action UPDATE_TOTAL_SALES");
   return {type:'UPDATE_TOTAL_SALES',total_Sales:total_Sales}
 }
+
+export const updateHotelSalesAnalysis = (sales_analysis) =>{
+  console.log("Action UPDATE_HOTEL_ANALYSIS");
+  return {
+    type:"UPDATE_HOTEL_ANALYSIS",
+    hotel_analysis_data:sales_analysis
+  }
+}
--- kayak_frontend/src/components/subcomponents/hotel_graphs.js
+++ kayak_frontend/src/components/subcomponents/hotel_graphs.js
@@ -1,10 +1,20 @@
 import React,{ Component } from 'react';
+import {connect} from 'react-redux';
 import AdminDashboardHeader from './../headers/admin_dashboard_header';
-
 import './../../images/admin.css';
+import BarChart from './../graph/bar_chart';
 
 class HotelGraphs extends Component {
 
+  getAdminDashBoardGraph(data, label_name, header_text){
+    var data={
+        labels: data.hotels,
+        datasets:data.sales,
+        labelName:label_name,
+        header_text:header_text
+    }
+    return (<BarChart data={data}/>)
+  }
 
   render() {
     return (
@@ -15,18 +25,23 @@ class HotelGraphs extends Component {
                 </div>
 
                 <br></br>
-                <h1>Graph 1 :(description of graph) </h1>
+                <h3 className="hotel-graph-header">Top 10 sales of properties </h3>
                 <div className="car-graph-1">
-
+                    {this.getAdminDashBoardGraph(this.props.hotel_analysis_data[0].top_ten_hotel_sales,
+                    " Sales Report"," Top 10 properties in terms of sales")}
                 </div>
                 <br></br>
-                <h1>Graph 2 :(description of graph) </h1>
+                <h3 className="hotel-graph-header">Top 10 sales in terms of city</h3>
                 <div className="car-graph-2">
+                {this.getAdminDashBoardGraph(this.props.hotel_analysis_data[0].top_ten_hotel_sales,
+                " Sales Report"," Top 10 properties in terms of sales")}
                 </div>
 
                 <br></br>
-                <h1>Graph 3 :(description of graph) </h1>
+                <h3 className="hotel-graph-header">Top 10 properties sales in terms of host/hotel owner </h3>
                 <div className="car-graph-3">
+                {this.getAdminDashBoardGraph(this.props.hotel_analysis_data[0].top_ten_hotel_sales,
+                " Sales Report"," Top 10 properties in terms of sales")}
                 </div>
 
             </div>
@@ -35,5 +50,11 @@ class HotelGraphs extends Component {
 }
 
 
+function mapStateToProps(state) {
+    console.log("Hotel graph mapStateToProps:",state.admin_reducer.hotel_analysis_data);
+    return {
+        hotel_analysis_data: state.admin_reducer.hotel_analysis_data,
+    };
 
-export default HotelGraphs;
+}
+export default connect(mapStateToProps,null)(HotelGraphs);
