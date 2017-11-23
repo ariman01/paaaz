b053e70915e81da84c628c91e873131f60cb8042
akashgupta2903 <akash.gupta@gmail.com>
2017-11-22 17:01:03 -0800

    adding api for hotel analysis

--- kayak_backend/routes/analysis.js
+++ kayak_backend/routes/analysis.js
@@ -4,12 +4,13 @@ var kafka = require('./../kafka/client');
 
 router.post('/one', function(req, res, next) {
 
-           	
+
 		 		var date = new Date(req.body.date);
 		 		var year = date.getFullYear();
 		 		console.log("Year entered is:"+year);
 
-    kafka.make_request('admin_analysis_hotel', {"year" : year}, function(err,result){
+    kafka.make_request('admin_analysis_hotel', {"year" : 2017}, function(err,result){
+        console.log("err",err,"result",result.finalResult[0]);
         if(err){
             console.log(err);
         }else {
@@ -17,14 +18,14 @@ router.post('/one', function(req, res, next) {
         }
     });
 });
-	
+
 
 // Analysis 2
 
 router.post('/two', function(req, res, next) {
 
-           	
-		 		
+
+
     kafka.make_request('admin_analysis2_hotel', {}, function(err,result){
         if(err){
             console.log(err);
@@ -36,8 +37,8 @@ router.post('/two', function(req, res, next) {
 
 router.post('/three', function(req, res, next) {
 
-           	
-		 		
+
+
     kafka.make_request('admin_analysis3_hotel', {}, function(err,result){
         if(err){
             console.log(err);
